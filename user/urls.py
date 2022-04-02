e684868634ec3a39cdba0276363902f58d17ba9e
TaoXU0 <464612404@qq.com>
2022-04-02 20:51:57 +0800

    Add login view and index view.

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('reg', views.reg_view)
+    path('reg', views.reg_view),
+    path('login', views.login_view)
 ]
--- user/views.py
+++ user/views.py
@@ -26,7 +26,60 @@ def reg_view(request):
         # 2. If the username valid
         old_users = User.objects.filter(username=username)
         if old_users:
-            return HttpResponse('Username is registered.')
+            return HttpResponse('Username has been registered.')
         # 3. Store data
-        User.objects.create(username=username, password=password_m)
-        return HttpResponse('Successfully register')
+        try:
+            user = User.objects.create(username=username, password=password_m)
+        except Exception as e:
+            print('--create user is error %s' % e)
+            return HttpResponse('Username has been registered.')
+        # Create session
+        request.session['username'] = username
+        request.session['uid'] = user.id
+
+        return HttpResponseRedirect('/index')
+
+
+def login_view(request):
+    # login
+    # GET return page
+    if request.method == 'GET':
+        # check login state
+        if request.session.get('username') and request.session.get('uid'):
+            return HttpResponseRedirect('/index')
+        c_username = request.COOKIES.get('username')
+        c_uid = request.COOKIES.get('uid')
+        if c_username and c_uid:
+            request.session['username'] = c_username
+            request.session['uid'] = c_uid
+            return HttpResponseRedirect('/index')
+        return render(request, 'user/login.html')
+    # POST process data
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except Exception as e:
+            print('login user error is %s' % e)
+            return HttpResponse('User name or password is wrong')
+
+        # Compare the password
+        m = hashlib.md5()
+        m.update(password.encode())
+
+        if m.hexdigest() != user.password:
+            return HttpResponse('User name or password is wrong')
+
+        # create session
+        request.session['username'] = username
+        request.session['uid'] = user.id
+
+        resp = HttpResponseRedirect('/index')
+        # if user click remember username then add cookies
+        if 'remember' in request.POST:
+            resp.set_cookie(key='username', value=username, max_age=3*3600*24)
+            resp.set_cookie(key='uid', value=user.id, max_age=3*3600*24)
+
+        return resp
